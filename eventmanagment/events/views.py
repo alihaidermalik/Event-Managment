a18ffca53edbc4e4aa1568db7d04ed947e25edac
Ali Haider Malik <mr.alihaidermalik@gmail.com>
2021-03-22 14:46:42 +0500

    Configure “post” endpoint.

--- eventmanagment/events/views.py
+++ eventmanagment/events/views.py
@@ -7,10 +7,31 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from django.utils.timezone import datetime
 
 class EventList(generics.ListAPIView):
     queryset = Event.objects.prefetch_related('attendees').select_related("owner").all()
     serializer_class = EventListSerializer
+
+    def post(self, request):
+        owner = request.user
+        title = request.data.get('title', 'No name')
+        date = request.data.get('date', datetime.today())
+        description = request.data.get('description', '')
+        location = request.data.get('location', '')
+        event = Event.objects.create(title=title, date=date, description=description, location=location, owner=owner)
+        attendees = request.data.get('attendees', None)
+        if attendees:
+            for guest in attendees:
+                try:
+                    attendee = User.objects.get(username=guest)
+                    event.attendees.add(attendee)
+                except:
+                    return Response({"detail":"Invalid attendee name "+guest})
+
+        return Response({"detail":"created sucessfully"})
+
+
 
 
 class EventDetail(generics.RetrieveAPIView):
@@ -39,7 +60,7 @@ class EventDetail(generics.RetrieveAPIView):
             if user == event.owner:
                 event.title = request.data.get('title', event.title)
                 event.date = request.data.get('date', event.date)
-                event.description = request.data.get('date', event.description)
+                event.description = request.data.get('description', event.description)
                 event.location = request.data.get('location', event.location)
                 event.save()
                 attendees = request.data.get('attendees', None)
