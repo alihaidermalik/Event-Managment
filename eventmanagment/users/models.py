d81637cd7e049dfef9751493d81d4925a65aef18
Ali Haider Malik <mr.alihaidermalik@gmail.com>
2021-03-22 11:50:52 +0500

    Configure user model and authentication.

--- eventmanagment/users/models.py
+++ eventmanagment/users/models.py
@@ -2,7 +2,8 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-
+from django.core.validators import RegexValidator
+from django.db import models
 
 class User(AbstractUser):
     """Default user for EventManagment."""
@@ -11,6 +12,8 @@ class User(AbstractUser):
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    mobile_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
